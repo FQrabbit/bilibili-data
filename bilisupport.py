43e851e39b2699df71020b5f02131c691f17644b
TimeCompass <hourglassmiku@gmail.com>
2016-12-17 17:19:15 +0800

    API list

---
+++ bilisupport.py
@@ -0,0 +1,26 @@
+'''API列表'''
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from pymongo import MongoClient
+
+DATABASE = MongoClient('mongodb://127.0.0.1:27017/', connect=False)
+AVIDLIST = DATABASE['bilibili-data']['SubmitVideos']
+DANMAKULIST = DATABASE['bilibili-data']['DanmakuData']
+REPLYLIST = DATABASE['bilibili-data']['CommentData']
+ACCOUNTLIST = DATABASE['bilibili-data']['SpaceInfo']
+
+# APPKEY = '12737ff7776f1ade'
+APPKEY = '8e9fc618fbd41e28'
+
+# CID_DANMAKU.format(cid)
+CID_DANMAKU = 'http://comment.bilibili.com/{0}.xml'
+# {'aid': aid}
+API_PAGELIST = 'http://www.bilibili.com/widget/getPageList?'
+# {'mid': mid, 'pagesize': pagesize, 'tid': tid, 'page': page}
+API_SUBMITVIDEOS = 'http://space.bilibili.com/ajax/member/getSubmitVideos?'
+# {'type': 'json', 'appkey': APPKEY, 'id': aid} & cookies needed
+API_VIDEOSTATUS = 'http://api.bilibili.com/view?'
+
+HEADERS = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 \
+    (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36"}
