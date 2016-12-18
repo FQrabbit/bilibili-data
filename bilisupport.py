516c75f1b5ca28d6cf3cf5e95f55879ea235d257
TimeCompass <hourglassmiku@gmail.com>
2016-12-18 14:57:40 +0800

    添加评论API

--- bilisupport.py
+++ bilisupport.py
@@ -16,14 +16,26 @@ APPKEY = '8e9fc618fbd41e28'
 
 # CID_DANMAKU.format(cid)
 CID_DANMAKU = 'http://comment.bilibili.com/{0}.xml'
+
 # {'aid': aid}
 API_PAGELIST = 'http://www.bilibili.com/widget/getPageList?'
+
 # {'mid': mid, 'pagesize': pagesize, 'tid': tid, 'page': page}
 API_SUBMITVIDEOS = 'http://space.bilibili.com/ajax/member/getSubmitVideos?'
+
 # {'type': 'json', 'appkey': APPKEY, 'id': aid} & cookies needed
 API_VIDEOSTATUS = 'http://api.bilibili.com/view?'
-# {'mid': mid} {'Referer': 'http://space.bilibili.com/{mid}/'}
+
+# {'mid': mid} {'Referer': 'http://space.bilibili.com/{mid}/'} POST method
 API_SPACE = 'http://space.bilibili.com/ajax/member/GetInfo'
+
+# {'jsonp': jsonp, 'type': 1, 'sort': 0, 'oid': oid, 'pn': page, 'nohot': 1}
+API_REPLY = 'http://api.bilibili.com/x/v2/reply?'
 
 HEADERS = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 \
     (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36"}
+
+PROXIES = {
+    'http': 'socks5://127.0.0.1:1080',
+    'https': 'socks5://127.0.0.1:1080'
+}
