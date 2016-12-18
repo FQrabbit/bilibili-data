ff092954619e11b26245abecb7984fffc0604c97
TimeCompass <hourglassmiku@gmail.com>
2016-12-18 14:57:29 +0800

    对API限制的临时解决措施

--- space.py
+++ space.py
@@ -2,8 +2,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from multiprocessing.dummy import Pool as ThreadPool
-from bilisupport import ACCOUNTLIST, ERRORLIST, API_SPACE
+import time
+from bilisupport import ACCOUNTLIST, API_SPACE, ERRORLIST, HEADERS
 import requests
 
 
@@ -13,17 +13,29 @@ def getspaceinfo(headers, form):
         jsondata = requests.post(url=API_SPACE, headers=headers, data=form)
     except TimeoutError:
         ERRORLIST.insert(form)
+    if jsondata.status_code != 200:
+        ERRORLIST.insert(form)
+        print(u"API 返回 403")
+        return 0
     data = jsondata.json().get('data')
+    if data == "服务器遇到了一些问题":
+        print(u"服务器遇到了一些问题")
+        ERRORLIST.insert(form)
+        return 0
     ACCOUNTLIST.update(form, {'$set': data}, upsert=True)
+    print(form)
+    return 1
 
 
 if __name__ == '__main__':
-    MULTIPOOL = ThreadPool(2)
-    for mid in range(1, 60631040):
+    for mid in range(12977, 60631040):
         spaceurl = 'http://space.bilibili.com/{mid}/'.format(mid=mid)
+        HEADERS['Referer'] = spaceurl
         postheaders = {'Referer': spaceurl}
         postdata = {'mid':"{0}".format(mid)}
-        print(mid)
-        MULTIPOOL.apply_async(getspaceinfo, (postheaders, postdata, ))
-    MULTIPOOL.close()
-    MULTIPOOL.join()
+        if getspaceinfo(HEADERS, postdata):
+            pass
+        else:
+            print(u"屏蔽判定，暂停1分钟")
+            time.sleep(61)
+            mid -= 1
