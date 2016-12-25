4e345e8a74c4af8dccae7fe467827694f067423b
TimeCompass <hourglassmiku@gmail.com>
2016-12-26 00:12:17 +0800

    use bilisupport

--- distinct.py
+++ distinct.py
@@ -2,10 +2,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from pymongo import MongoClient
-
-DATABASE = MongoClient('mongodb://127.0.0.1:27017/', connect=False)
-AVIDLIST = DATABASE['bilibili-data']['SubmitVideos']
+from bilisupport import AVIDLIST
 
 for aid in open('videoaid.csv', 'r'):
     aid = int(aid.strip('\n').strip('\r'))
